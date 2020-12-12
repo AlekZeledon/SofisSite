cc4b61fefa886b58b6836d33d899a2527a50d30c
Alek Z <alekzeledon@gmail.com>
2020-12-11 21:26:11 -0800

    Almost Final Version

--- script.js
+++ script.js
@@ -6,108 +6,474 @@ $(document).ready(function() {
 
     random_quote[0] = {
       "quotenumber": 1,
-      "quotesentence": "People die when they are killed.",
-      "quotecharacter": "- Emiya Shirou,",
-      "quoterandom": " Fate/stay night"
+      "quotesentence": "Voici mon secret. Il est très simple: on ne voit bien qu'avec le cœur. L'essentiel est invisible pour les yeux.",
+      "quotecharacter": "- The Little Prince",
     }
 
     random_quote[1] = {
       "quotenumber": 2,
-      "quotesentence": "The fake is of far greater value. In its deliberate attempt to be real, it's more real than the real thing.",
-      "quotecharacter": "- Kaiki Deishū,",
-      "quoterandom": " Nisemonogatari"
+      "quotesentence": "We’re all mad here",
+      "quotecharacter": "- Cheshire Cat",
     }
 
     random_quote[2] = {
       "quotenumber": 3,
-      "quotesentence": "Mankind’s greatest fear is Mankind itself.",
-      "quotecharacter": "- Gendo Ikari,",
-      "quoterandom": " Neon Genesis Evangelion"
+      "quotesentence": "My thoughts are stars I cannot fathom into constellations.",
+      "quotecharacter": "- John Green",
     }
 
     random_quote[3] = {
       "quotenumber": 4,
-      "quotesentence": "If you act only on what you should do without heed for what you want to do, you're nothing more than a machine, a phenomenon.",
-      "quotecharacter": "- Natalia Kaminski,",
-      "quoterandom": " Fate/Zero"
+      "quotesentence": "I declare after all there is no enjoyment like reading! How much sooner one tires of any thing than of a book! -- When I have a house of my own, I shall be miserable if I have not an excellent library.",
+      "quotecharacter": "- Jane Austen",
     }
 
     random_quote[4] = {
       "quotenumber": 5,
-      "quotesentence": "It's a mistake to choose your path based upon the sins that you carry. We don't choose our path based on the sins that we carry, but we must bear our sins on the path we choose. ",
-      "quotecharacter": "- Aozaki Touko,",
-      "quoterandom": " Kara no Kyoukai"
+      "quotesentence": "'And so the lion fell in love with the lamb…' he murmured. I looked away, hiding my eyes as I thrilled to the word. 'What a stupid lamb' I sighed.'What a sick, masochistic lion'",
+      "quotecharacter": "- Stephenie Meyer",
     }
 
     random_quote[5] = {
       "quotenumber": 6,
-      "quotesentence": "A lie that can't be disproven is no different from the truth.",
-      "quotecharacter": "- Kraft Lawrence,",
-      "quoterandom": " Ookami to Koushinryou"
+      "quotesentence": "The bird, the pin, the song, the berries, the watch, the cracker, the dress that burst into flames. I am the mockingjay. The one that survived despite the Capitol's plans. The symbol of the rebellion.",
+      "quotecharacter": "- Suzanne Collins",
     }
 
     random_quote[6] = {
       "quotenumber": 7,
-      "quotesentence": "I can only say that the human curiosity is something completely irrational.",
-      "quotecharacter": "- Kyubee,",
-      "quoterandom": " Mahou Shoujo Madoka★Magica"
+      "quotesentence": "And now here is my secret, a very simple secret: It is only with the heart that one can see rightly; what is essential is invisible to the eye.",
+      "quotecharacter": "- The Little Prince",
     }
 
     random_quote[7] = {
       "quotenumber": 8,
-      "quotesentence": "The day before yesterday, I saw a rabbit. Yesterday, it was a deer. And today, It’s you.",
-      "quotecharacter": "- Ichinose Kotomi,",
-      "quoterandom": " Clannad"
+      "quotesentence": "All grown-ups were once children... but only few of them remember it.",
+      "quotecharacter": "- The Little Prince",
     }
 
     random_quote[8] = {
       "quotenumber": 9,
-      "quotesentence": "Only God knows who shall be the victor. I can only do the best I can.",
-      "quotecharacter": "- Takane Shijou,",
-      "quoterandom": " The iDOLM@STER"
+      "quotesentence": "It is the time you have wasted for your rose that makes your rose so important.",
+      "quotecharacter": "- The Little Prince",
     }
 
     random_quote[9] = {
       "quotenumber": 10,
-      "quotesentence": "Those aren’t tears of sadness. They’re tears of happiness, right? Let us make haste, lest the wind ceases.",
-      "quotecharacter": "- Hidenori Tabata,",
-      "quoterandom": " Danshi Koukousei no Nichijou"
+      "quotesentence": "What makes the desert beautiful,' said the little prince, 'is that somewhere it hides a well...",
+      "quotecharacter": "- The Little Prince",
     }
 
     random_quote[10] = {
       "quotenumber": 11,
-      "quotesentence": "Poi!",
-      "quotecharacter": "- Yuudachi,",
-      "quoterandom": " Kantai Collection"
+      "quotesentence": "The most beautiful things in the world cannot be seen or touched, they are felt with the heart.",
+      "quotecharacter": "- The Little Prince",
     }
 
     random_quote[11] = {
       "quotenumber": 12,
-      "quotesentence": "If you forced someone to do something they didn’t want to do, they’d just end up hating life.",
-      "quotecharacter": "- Chihiro Sengoku,",
-      "quoterandom": " Sakurasou no Pet na Kanojo"
+      "quotesentence": "You - you alone will have the stars as no one else has them...In one of the stars I shall be living. In one of them I shall be laughing. And so it will be as if all the stars were laughing, when you look at the sky at night...You - only you - will have stars that can laugh.",
+      "quotecharacter": "- The Little Prince",
     }
 
     random_quote[12] = {
       "quotenumber": 13,
-      "quotesentence": "There are things that cannot be taken back. But the world will keep on spinning whether you laugh or you cry. High school life will eventually end too.",
-      "quotecharacter": "- Hachiman Hikigaya,",
-      "quoterandom": " Yahari Ore no Seishun Love Comedy wa Machigatteiru."
+      "quotesentence": "If you love a flower that lives on a star, it is sweet to look at the sky at night. All the stars are a-bloom with flowers...",
+      "quotecharacter": "- The Little Prince",
     }
 
     random_quote[13] = {
       "quotenumber": 14,
-      "quotesentence": "There's something wrong with people who seek reality in fiction.",
-      "quotecharacter": "- Kyon,",
-      "quoterandom": " Suzumiya Haruhi no Yuuutsu"
+      "quotesentence": "It does not do well to dwell on dreams and forget to live.",
+      "quotecharacter": "- Albus Dumbledor",
     }
 
     random_quote[14] = {
       "quotenumber": 15,
-      "quotesentence": "Don't be distracted by the what-if's, should-have's, and if-only's. The one thing you choose for yourself - that is the truth of your universe.",
-      "quotecharacter": "- Kamina,",
-      "quoterandom": " Tengen Toppa Gurren Lagann"
+      "quotesentence": "But you know, happiness can be found even in the darkest of times, if one only remembers to turn on the light.",
+      "quotecharacter": "- Albus Dumbledore",
     }
+
+    random_quote[15] = {
+      "quotenumber": 16,
+      "quotesentence": "It matters not what someone is born, but what they grow to be.",
+      "quotecharacter": "- Albus Dumbledore",
+    }
+
+    random_quote[16] = {
+      "quotenumber": 17,
+      "quotesentence": "Anything’s possible if you’ve got enough nerve.",
+      "quotecharacter": "- Ginny Weasley",
+    }
+
+    random_quote[17] = {
+      "quotenumber": 18,
+      "quotesentence": "When in doubt, go to the library.",
+      "quotecharacter": "- Ron Weasley",
+    }
+
+    random_quote[18] = {
+      "quotenumber": 19,
+      "quotesentence": "Don’t let the muggles get you down.",
+      "quotecharacter": "- Ron Weasley",
+    }
+
+    random_quote[19] = {
+      "quotenumber": 20,
+      "quotesentence": "The ones that love us never really leave us.",
+      "quotecharacter": "- Sirius Black",
+    }
+
+    random_quote[20] = {
+      "quotenumber": 21,
+      "quotesentence": "Do not pity the dead, Harry, pity the living. And above all pity those who live without love.",
+      "quotecharacter": "- Albus Dumbledore",
+    }
+
+    random_quote[21] = {
+      "quotenumber": 22,
+      "quotesentence": "It is impossible to manufacture or imitate love.",
+      "quotecharacter": "- Professor Slughorn",
+    }
+
+    random_quote[22] = {
+      "quotenumber": 23,
+      "quotesentence": "Where your treasure is, there will your heart be also.",
+      "quotecharacter": "- Albus Dumbledore",
+    }
+
+    random_quote[23] = {
+      "quotenumber": 24,
+      "quotesentence": "When you have seen as much of life as I have, you will not underestimate the power of obsessive love.",
+      "quotecharacter": "- Horace Slughorn",
+    }
+
+    random_quote[24] = {
+      "quotenumber": 25,
+      "quotesentence": "Dumbledore would have been happier than anybody to think that there was a little more love in the world.",
+      "quotecharacter": "- Professor McGonagall",
+    }
+
+    random_quote[25] = {
+      "quotenumber": 26,
+      "quotesentence": "Differences of habit and language are nothing at all if our aims are identical and our hearts are open.",
+      "quotecharacter": "- Albus Dumbledore",
+    }
+
+    random_quote[26] = {
+      "quotenumber": 27,
+      "quotesentence": "From the tip of his wand burst the silver doe; she landed on the office floor, bounded once across the office and soared out of the window. Dumbledore watched her fly away, and as her silvery glow faded he turned back to Snape, and his eyes were full of tears. 'After all this time?' 'Always,' said Snape.",
+      "quotecharacter": "- Severus Snape, Albus Dumbledore",
+    }
+
+    random_quote[27] = {
+      "quotenumber": 28,
+      "quotesentence": "Until the very end.",
+      "quotecharacter": "- James Potter",
+    }
+
+    random_quote[28] = {
+      "quotenumber": 29,
+      "quotesentence": "Fear of a name only increases fear of the thing itself.",
+      "quotecharacter": "- Hermione Granger",
+    }
+
+    random_quote[29] = {
+      "quotenumber": 30,
+      "quotesentence": "I like the night. Without the dark, we’d never see the stars.",
+      "quotecharacter": "- Isabella Swan",
+    }
+
+    random_quote[30] = {
+      "quotenumber": 31,
+      "quotesentence": "I have always loved you, and I will always love you. I was thinking of you, seeing your face in my mind, every second that I was away",
+      "quotecharacter": "- Edward Cullen",
+    }
+
+    random_quote[31] = {
+      "quotenumber": 32,
+      "quotesentence": "You are the only one who has ever touched my heart. It will always be yours.",
+      "quotecharacter": "- Edward Cullen",
+    }
+
+    random_quote[32] = {
+      "quotenumber": 33,
+      "quotesentence": "No measure of time with you will be long enough, but we'll start with forever.",
+      "quotecharacter": "- Edward Cullen",
+    }
+
+    random_quote[33] = {
+      "quotenumber": 34,
+      "quotesentence": "You are the most important thing to me now. The most important thing to me ever.",
+      "quotecharacter": "- Edward Cullen",
+    }
+
+    random_quote[34] = {
+      "quotenumber": 35,
+      "quotesentence": "Three Words. Eight Letters. Say It And I'm Yours. ",
+      "quotecharacter": "- Blair Waldorf",
+    }
+
+    random_quote[35] = {
+      "quotenumber": 36,
+      "quotesentence": "I love you more and more every day if it’s even possible to love someone that much.",
+      "quotecharacter": "- Blair Waldorf",
+    }
+
+    random_quote[36] = {
+      "quotenumber": 37,
+      "quotesentence": "What we have is a great love. It’s complicated. Intense. All-consuming. No matter what we do and how much we fight, it’ll always pull us in.",
+      "quotecharacter": "- Blair Waldorf",
+    }
+
+    random_quote[37] = {
+      "quotenumber": 38,
+      "quotesentence": "I like shiny things, but I'd marry you with paper rings",
+      "quotecharacter": "- Taylor Swift",
+    }
+
+    random_quote[38] = {
+      "quotenumber": 39,
+      "quotesentence": "Hell is when I fight with you but we can patch it up good",
+      "quotecharacter": "- Taylor Swift",
+    }
+
+    random_quote[39] = {
+      "quotenumber": 40,
+      "quotesentence": "Darling, I fancy you.",
+      "quotecharacter": "- Taylor Swift",
+    }
+
+    random_quote[40] = {
+      "quotenumber": 41,
+      "quotesentence": "I don't wanna look at anything else now that I saw you.",
+      "quotecharacter": "- Taylor Swift",
+    }
+
+    random_quote[41] = {
+      "quotenumber": 42,
+      "quotesentence": "Can we always be this close forever and ever?",
+      "quotecharacter": "- Taylor Swift",
+    }
+
+    random_quote[42] = {
+      "quotenumber": 43,
+      "quotesentence": "And I'm highly suspicious that everyone who sees you wants you",
+      "quotecharacter": "- Taylor Swift",
+    }
+
+    random_quote[43] = {
+      "quotenumber": 44,
+      "quotesentence": "I've loved you three summers now, honey, but I want 'em all.",
+      "quotecharacter": "- Taylor Swift",
+    }
+
+    random_quote[44] = {
+      "quotenumber": 45,
+      "quotesentence": "I hate accidents except when we went from friends to this",
+      "quotecharacter": "- Taylor Swift",
+    }
+
+    random_quote[45] = {
+      "quotenumber": 46,
+      "quotesentence": "I don't think you can define love.",
+      "quotecharacter": "- Harry Styles",
+    }
+
+    random_quote[46] = {
+      "quotenumber": 47,
+      "quotesentence": "Don't choose the one who is beautiful to the world. But rather, choose the one who makes your world beautiful.",
+      "quotecharacter": "- Harry Styles",
+    }
+
+    random_quote[47] = {
+      "quotenumber": 48,
+      "quotesentence": "If one heart fits another like a puzzle piece, maybe you could be the missing half of me.",
+      "quotecharacter": "- Harry Styles",
+    }
+
+    random_quote[48] = {
+      "quotenumber": 49,
+      "quotesentence": "I like having someone I can spoil.",
+      "quotecharacter": "- Harry Styles",
+    }
+
+    random_quote[49] = {
+      "quotenumber": 50,
+      "quotesentence": "I love you to the moon and back",
+      "quotecharacter": "- Alek",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "I wanna just cuddle you forever. I'll hug you tightly and never let you go",
+      "quotecharacter": "- Alek, 2015",
+    }
+
+    random_quote[51] = {
+      "quotenumber": 52,
+      "quotesentence": "I think your voice sounds adorable",
+      "quotecharacter": "- Alek, 2014",
+    }
+
+    random_quote[52] = {
+      "quotenumber": 53,
+      "quotesentence": "I feel like if you were my gf or something. I'd say best friend but I do like you so it's more of the way I'd act with a gf. Idk, you're just cool and fun to talk to so I just hope I don't annoy you.",
+      "quotecharacter": "- Alek, 2014",
+    }
+
+    random_quote[53] = {
+      "quotenumber": 55,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[54] = {
+      "quotenumber": 55,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+    random_quote[50] = {
+      "quotenumber": 51,
+      "quotesentence": "",
+      "quotecharacter": "- ",
+    }
+
+
+
 
 
     
